fe780966bdb476d9ea442001a913b0cd06ae0fab
AAlvarez <aalvarez59@gmail.com>
2024-06-18 13:54:24 -0600

    initializer for ahorcado game

---
+++ ahorcado/initializer.py
@@ -0,0 +1,6 @@
+from Ahorcado import Ahorcado
+lista_palabras = ["manzana", "sol", "libro", "casa", "perro", "gato", "mesa", "silla", "computadora", "celular", "amor", "paz", "felicidad", "tristeza", "alegría", "esperanza", "sueño", "meta", "objetivo", "ilusión"]
+
+juego_ahorcado = Ahorcado(lista_palabras)
+
+juego_ahorcado.iniciar_juego()
